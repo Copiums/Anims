b6c8f4f75c91725e1020ac67608b6404d1f08563
Copium <119388242+Copiums@users.noreply.github.com>
2024-04-09 19:45:34 -0400

    Create Goofy.lua

---
+++ Goofy.lua
@@ -0,0 +1,8 @@
+while true do
+    Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=4417977954"
+    Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=4417978624"
+    Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=10921162768"
+    Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=4417979645"
+    Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=10921137402"
+    Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=10921136539"
+end
