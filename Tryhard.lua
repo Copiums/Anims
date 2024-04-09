613763674e79a265e08b20062b00930f9272a839
Copium <119388242+Copiums@users.noreply.github.com>
2024-04-09 19:46:27 -0400

    Create Tryhard.lua

---
+++ Tryhard.lua
@@ -0,0 +1,8 @@
+while true do
+    Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=10921301576"
+    Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=10921302207"
+    Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=10921162768"
+    Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=10921157929"
+    Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=10921137402"
+    Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=10921136539"
+end
