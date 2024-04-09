94c189f70d27f7a5ebb8a83d72d416b0c0798909
Copium <119388242+Copiums@users.noreply.github.com>
2024-04-09 19:44:18 -0400

    Create TanqR.lua

---
+++ TanqR.lua
@@ -0,0 +1,9 @@
+while true do
+    local Animate = game:GetService("Players").LocalPlayer.Character.Animate
+    Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=10921034824"
+    Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=10921036806"
+    Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=10921312010"
+    Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=10921306285"
+    Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=10921242013"
+    Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=10921136539"
+end
