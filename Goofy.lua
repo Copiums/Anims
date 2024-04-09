0ffd9048a6e0272aabdea1c4bc9824eec04dae67
Copium <119388242+Copiums@users.noreply.github.com>
2024-04-09 19:46:56 -0400

    Update Goofy.lua

--- Goofy.lua
+++ Goofy.lua
@@ -1,4 +1,5 @@
 while true do
+    local Animate = game:GetService("Players").LocalPlayer.Character.Animate
     Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=4417977954"
     Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=4417978624"
     Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=10921162768"
